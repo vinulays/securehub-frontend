488e2fed385ee773b07fa9b164416fd92623dbf0
Vinula Senarathne <vinulapc@gmail.com>
2026-06-27 12:46:44 +0530

    chore(git): prevent throwing error when API_URL is not found

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,19 +1,15 @@
 import type { InternalAxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
-import { API_ROUTES } from '@/constants/api';
+import { API_BASE_URL, API_ROUTES } from '@/constants/api';
 import { ROUTES } from '@/constants/routes';
 
 interface RetryConfig extends InternalAxiosRequestConfig {
   _retry?: boolean;
 }
 
-if (!process.env.NEXT_PUBLIC_API_URL) {
-  throw new Error('NEXT_PUBLIC_API_URL is not defined');
-}
-
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
